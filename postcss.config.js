cc6bd38a5bdb22cd1c35317368fed3c4cb5b93dd
DySyre <119650160+DySyre@users.noreply.github.com>
2025-12-10 23:43:45 +0800

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 const tailwindcss = require('@tailwindcss/postcss');
 
 module.exports = {
@@ -5,4 +6,12 @@ module.exports = {
     tailwindcss(),
     require('autoprefixer'),
   ],
+=======
+/** @type {import('postcss').Config} */
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+>>>>>>> Stashed changes
 }
